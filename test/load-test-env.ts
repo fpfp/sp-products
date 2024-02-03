60b5e4e37a2909b2bfa868d56520e69171afcfc9
Fabio <f.pozzati@notomia.com>
2024-02-03 16:11:01 +0100

    test(products): restore test env to handle vars from file or from env

--- test/load-test-env.ts
+++ test/load-test-env.ts
@@ -1,2 +1,22 @@
+import { readFileSync, existsSync } from 'fs';
+import { join } from 'path';
+
+const filePath = join(__dirname, `../.env`);
+if (existsSync(filePath)) {
+  try {
+    const envFile = readFileSync(filePath, 'utf-8');
+    const lines = envFile.split('\n');
+
+    for (const line of lines) {
+      if (line.trim() === '' || line.trim().startsWith('#')) continue;
+      const [key, value] = line.split('=');
+
+      process.env[key.trim()] = value.trim();
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Could not read .env file`);
+  }
+}
 process.env.NODE_ENV = 'test';
 process.env.DB_DATABASE = `${process.env.DB_DATABASE}_test`;
